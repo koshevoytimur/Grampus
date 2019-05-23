e84abdd8d6ad83af30bb33c9dfdfed0d970493b2
Timur Koshevoy <timurkoshevoy@gmail.com>
2019-05-23 12:55:18 +0300

    minor design fixes

--- Grampus/Controllers/SignInViewController.swift
+++ Grampus/Controllers/SignInViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: - Outlets
     @IBOutlet weak var _userName: UITextField!
@@ -20,8 +20,96 @@ class SignInViewController: UIViewController {
     // MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        _userName.delegate = self
+        _password.delegate = self
+        
+        SetUpOutlets()
+        dismissKeyboardOnTap()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeNotifications()
     }
 
+    func SetUpOutlets() {
+    
+        _userName.layer.shadowColor = UIColor.darkGray.cgColor
+        _userName.layer.shadowOffset = CGSize(width: 3, height: 3)
+        _userName.layer.shadowRadius = 5
+        _userName.layer.shadowOpacity = 0.5
+        
+        _password.layer.shadowColor = UIColor.darkGray.cgColor
+        _password.layer.shadowOffset = CGSize(width: 3, height: 3)
+        _password.layer.shadowRadius = 5
+        _password.layer.shadowOpacity = 0.5
+        
+        _signInButton.layer.shadowColor = UIColor.darkGray.cgColor
+        _signInButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+        _signInButton.layer.shadowRadius = 5
+        _signInButton.layer.shadowOpacity = 0.5
+        
+        _signUpButton.layer.shadowColor = UIColor.darkGray.cgColor
+        _signUpButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+        _signUpButton.layer.shadowRadius = 5
+        _signUpButton.layer.shadowOpacity = 0.5
+        
+        _signInButton.layer.cornerRadius = 5
+        _signUpButton.layer.cornerRadius = 5
+    
+    }
+    
+    // Hide keyboard on tap.
+    func dismissKeyboardOnTap() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    // Hide Keyboard.
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    // Hide the keyboard when the return key pressed.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Notifications for moving view when keyboard appears.
+    func setUpNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    // Removing notifications.
+    func removeNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillHide() {
+        self.view.frame.origin.y = 0
+    }
+    
+    @objc func keyboardWillChange(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if _userName.isFirstResponder {
+                self.view.frame.origin.y = -keyboardSize.height + 100
+            } else if _password.isFirstResponder {
+                self.view.frame.origin.y = -keyboardSize.height + 100
+            }
+        }
+    }
+    
     // MARK: - Actions
     @IBAction func SignInButton(_ sender: UIButton) {
 //        let userName = _userName.text
@@ -35,9 +123,6 @@ class SignInViewController: UIViewController {
 //            
 //        }
     }
-    
-    
-    
     
 }
 
--- Grampus/Controllers/SignUpViewController.swift
+++ Grampus/Controllers/SignUpViewController.swift
@@ -9,18 +9,92 @@
 import UIKit
 import ValidationComponents
 
-class SignUpViewController: UIViewController {
-
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+    
     // MARK: - Outlets
     @IBOutlet weak var _userName: UITextField!
     @IBOutlet weak var _email: UITextField!
     @IBOutlet weak var _password: UITextField!
+    @IBOutlet weak var _signUpButton: UIButton!
     
     let predicate = EmailValidationPredicate()
     
     // MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        _userName.delegate = self
+        _email.delegate = self
+        _password.delegate = self
+        
+        SetUpOutlets()
+        dismissKeyboardOnTap()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeNotifications()
+    }
+    
+    func SetUpOutlets() {
+        
+        _userName.layer.shadowColor = UIColor.darkGray.cgColor
+        _userName.layer.shadowOffset = CGSize(width: 3, height: 3)
+        _userName.layer.shadowRadius = 5
+        _userName.layer.shadowOpacity = 0.5
+        
+        _password.layer.shadowColor = UIColor.darkGray.cgColor
+        _password.layer.shadowOffset = CGSize(width: 3, height: 3)
+        _password.layer.shadowRadius = 5
+        _password.layer.shadowOpacity = 0.5
+        
+        _email.layer.shadowColor = UIColor.darkGray.cgColor
+        _email.layer.shadowOffset = CGSize(width: 3, height: 3)
+        _email.layer.shadowRadius = 5
+        _email.layer.shadowOpacity = 0.5
+        
+        _signUpButton.layer.shadowColor = UIColor.darkGray.cgColor
+        _signUpButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+        _signUpButton.layer.shadowRadius = 5
+        _signUpButton.layer.shadowOpacity = 0.5
+        
+        _signUpButton.layer.cornerRadius = 5
+        
+    }
+    
+    // Notifications for moving view when keyboard appears.
+    func setUpNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    // Removing notifications.
+    func removeNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillHide() {
+        self.view.frame.origin.y = 0
+    }
+    
+    @objc func keyboardWillChange(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if _userName.isFirstResponder {
+                self.view.frame.origin.y = -keyboardSize.height + 100
+            } else if _email.isFirstResponder {
+                self.view.frame.origin.y = -keyboardSize.height + 100
+            } else if _password.isFirstResponder {
+                self.view.frame.origin.y = -keyboardSize.height + 100
+            }
+        }
     }
     
     // MARK: - Actions
@@ -30,6 +104,13 @@ class SignUpViewController: UIViewController {
         let email    = _email.text
         
         let emailFormatBool = predicate.evaluate(with: email)
+        
+        // Check lenght of user name
+        if let userName = _userName.text {
+            if userName.count < 2 {
+                showAlert("Name too short", "Write your correct name")
+            }
+        }
         
         // Email isEmpty check.
         if (email!.isEmpty) {
@@ -53,31 +134,35 @@ class SignUpViewController: UIViewController {
                 showAlert("Password too long", "Password shoud be less then 24 symbols")
             }
         }
-        
-        // Check lenght of user name
-        if let userName = _userName.text {
-            if userName.count < 2 {
-                showAlert("Name too short", "Write your correct name")
-            }
-        }
-        
-        
     }
     
     func showAlert(_ title: String, _ message: String) {
-    
+        
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-    
+        
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-    
+        
         alert.addAction(action)
-    
+        
         present(alert, animated: true, completion: nil)
-    
+        
     }
     
+    // Hide keyboard on tap.
+    func dismissKeyboardOnTap() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
     
+    // Hide Keyboard.
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
     
-    
+    // Hide the keyboard when the return key pressed.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
 }
