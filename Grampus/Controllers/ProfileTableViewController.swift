387009e1f79bb1d35619d0704637e1ed1b8f37cb
Timur Koshevoy <timurkoshevoy@gmail.com>
2019-05-24 12:53:29 +0300

    info and skills cells

--- Grampus/Controllers/ProfileTableViewController.swift
+++ Grampus/Controllers/ProfileTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ProfileTableViewController: UITableViewController {
-
+    
     @IBOutlet weak var _menuBarButton: UIBarButtonItem!
     @IBOutlet weak var _navigationBar: UINavigationBar!
     
@@ -22,13 +22,22 @@ class ProfileTableViewController: UITableViewController {
     
     //Achievement cell
     
+    //Information cell
+    @IBOutlet weak var _profileInformationLabel: UILabel!
+    //Skills cell
+    @IBOutlet weak var _profileSkillsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        navBarAppearance()
         
-        self.tableView.tableFooterView = UIView(frame: .zero)
-
+        navBarAppearance()
+        _profileInformationLabel.numberOfLines = 0
+        _profileSkillsLabel.numberOfLines = 0
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 120
+        
+        //self.tableView.tableFooterView = UIView(frame: .zero)
+        
         _navigationBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
         if revealViewController() != nil {
             _menuBarButton.target = self.revealViewController()
@@ -43,72 +52,67 @@ class ProfileTableViewController: UITableViewController {
         _profileImageView.layer.borderWidth = 1.5
         _profileImageView.layer.borderColor = UIColor.darkGray.cgColor
     }
-
+    
     // MARK: - Table view data source
-
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 2 || indexPath.row == 3 {
+            return UITableView.automaticDimension
+        } else {
+            return 120.0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 2 || indexPath.row == 3 {
+            return UITableView.automaticDimension
+        } else {
+            return 120.0
+        }
+    }
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 5
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    // MARK: - Actions
+    @IBAction func informationAddAction(_ sender: Any) {
+        let alert = UIAlertController(title: "Enter information about yourself:", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.text = self._profileInformationLabel.text
+        }
+        alert.addAction(UIAlertAction(title: "Save", style: .default, handler: { [weak alert] (_) in
+            let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+            self._profileInformationLabel.text = textField?.text
+            self.tableView.reloadData()
+            
+        }))
+        
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+        self.tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @IBAction func skillsAddAction(_ sender: Any) {
+        let alert = UIAlertController(title: "Enter your skills:", message: nil, preferredStyle: .alert)
+        
+        
+        alert.addTextField { (textField) in
+            textField.text = self._profileSkillsLabel.text
+        }
+        alert.addAction(UIAlertAction(title: "Save", style: .default, handler: { [weak alert] (_) in
+            let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+            self._profileSkillsLabel.text = textField?.text
+            self.tableView.reloadData()
+            
+        }))
+        
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+        self.tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
